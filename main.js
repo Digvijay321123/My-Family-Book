f415274b017b6d6db08de0fcf3f593c1176c7def
Digvijay321123 <93191578+Digvijay321123@users.noreply.github.com>
2021-11-18 09:27:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
+var names = ["Fmaily Book","GrandFather", "Sudhir Mane", "Digvijay Mane, "Priya Mane", "My Brother"];
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 6
+    if(i > numbers_of_family_member_in_array)
+      {
+          i = 0;
+      }
+
+    document.getElementById("family_member_image").src = images;
+    document.getElementById("family_member_name").innerHTML = names;
+}
