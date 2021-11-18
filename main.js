4f553bec8f62ca0403fd4d386402ab4502478bb5
Digvijay321123 <93191578+Digvijay321123@users.noreply.github.com>
2021-11-18 10:01:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg", "https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg", "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
+var images = ["https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg", "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
 var names = ["Family Book","GrandFather", "Sudhir Mane", "Digvijay Mane", "Priya Mane", "My Brother"];
 var i = 0;
 function update()
